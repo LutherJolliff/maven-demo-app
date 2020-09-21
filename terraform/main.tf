a7ed05216200a54bbee8f81da3cd71be5d2a47e2
Luther Jolliff <lutherjolliff@Luthers-MacBook-Pro.local>
2020-09-21 08:06:27 -0400

    change to govcloud

--- terraform/main.tf
+++ terraform/main.tf
@@ -6,8 +6,8 @@ provider "aws" {
 
 terraform {
   backend "s3" {
-    bucket = "terraform-remote-state-storage-s3-cynerge-infra-team-v1"
-    key    = "key-shipyard-maven/"
-    region = "us-east-2"
+    bucket = "fs-workflow-74836"
+    key    = "fs-workflow/state.tfvars"
+    region = "us-gov-east-1"
   }
 }
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,5 +1,5 @@
 variable "region" {
-  default = "us-east-1"
+  default = "us-gov-east-1"
 }
 
 variable "profile" {
